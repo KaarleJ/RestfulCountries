68a64433ed7aa92dbe60687a04da189e2be323e9
Kaarle Järvinen <kaarle46@gmail.com>
2024-02-13 21:29:02 +0200

    Fixed root page loading icon

--- rc-client/src/routes/root.tsx
+++ rc-client/src/routes/root.tsx
@@ -6,14 +6,19 @@ import CountryTable from "../components/CountryTable";
 
 const Root = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-  const search = searchParams.get("search")
+  const search = searchParams.get("search");
   const { countries, loading } = useCountries(search);
 
   return (
     <div className="relative flex  flex-col justify-start items-center h-5/6 py-10 mt-5">
-      <SearchBar className="absolute top-2 right-5" setParams={setSearchParams}/>
+      <SearchBar
+        className="absolute top-2 right-5"
+        setParams={setSearchParams}
+      />
       {loading ? (
-        <Loader size="48" />
+        <div className="flex items-center justify-center h-5/6">
+          <Loader size="48" />
+        </div>
       ) : (
         <CountryTable countries={countries} />
       )}
