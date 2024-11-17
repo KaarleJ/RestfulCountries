c09fb913ca4e680ec3cc3905525b0551d36db73a
Kaarle Järvinen <kaarle46@gmail.com>
2024-11-17 13:30:47 +0200

    Changed to clientside link

--- rc-client/src/components/CountryCard.tsx
+++ rc-client/src/components/CountryCard.tsx
@@ -1,3 +1,5 @@
+import Link from "./Link";
+
 interface CountryCardProps {
   name: {
     common: string;
@@ -11,11 +13,22 @@ interface CountryCardProps {
 
 const CountryCard = ({ name, flag }: CountryCardProps) => {
   return (
-    <a href={`/country/${name.common}`} className="bg-yellow-100 m-2 shadow-md py-5 px-16 flex flex-row justify-start items-center h-20 rounded-md w-full hover:text-cyan-500 hover:brightness-90 relative mt-5">
-        <h2 className="text-2xl mr-5 text-yellow-950 text-left truncate max-w-sm">{name.common}</h2>
-        <h3 className="text-lg text-yellow-800 text-left truncate md:flex">({name.official})</h3>
-      <img src={flag.svg} alt={flag.alt} className="w-20 h-12 shadow-md absolute top-5 right-5" />
-    </a>
+    <Link
+      to={`/country/${name.common}`}
+      className="bg-yellow-100 m-2 shadow-md py-5 px-16 flex flex-row justify-start items-center h-20 rounded-md w-full hover:text-cyan-500 hover:brightness-90 relative mt-5"
+    >
+      <h2 className="text-2xl mr-5 text-yellow-950 text-left truncate max-w-sm">
+        {name.common}
+      </h2>
+      <h3 className="text-lg text-yellow-800 text-left truncate md:flex">
+        ({name.official})
+      </h3>
+      <img
+        src={flag.svg}
+        alt={flag.alt}
+        className="w-20 h-12 shadow-md absolute top-5 right-5"
+      />
+    </Link>
   );
 };
 
