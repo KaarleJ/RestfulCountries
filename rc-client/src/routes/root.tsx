d1deaed0980f839cef0fdf99c023f8d7f43aaa64
Kaarle Järvinen <kaarle46@gmail.com>
2024-11-17 13:37:36 +0200

    Fixes to routing

--- rc-client/src/routes/root.tsx
+++ rc-client/src/routes/root.tsx
@@ -1,12 +1,13 @@
 import useCountries from "../hooks/useCountries";
 import Loader from "../components/Loader";
 import SearchBar from "../components/SearchBar";
-import { useSearchParams } from "react-router-dom";
+import { Outlet, useLocation, useSearchParams } from "react-router-dom";
 import CountryTable from "../components/CountryTable";
 import Paper from "../components/Paper";
 import AppHeader from "../components/AppHeader";
 
 const Root = () => {
+  const { pathname } = useLocation();
   const [searchParams, setSearchParams] = useSearchParams();
   const search = searchParams.get("search");
   const { countries, loading } = useCountries(search);
@@ -14,17 +15,19 @@ const Root = () => {
   return (
     <div className="flex flex-col justify-start items-center mx-36 mt-32">
       <AppHeader>
-        <SearchBar
-          setParams={setSearchParams}
-        />
+        <SearchBar setParams={setSearchParams} />
       </AppHeader>
       <Paper>
-        {loading ? (
-          <div className="flex items-center justify-center h-5/6">
-            <Loader size="48" />
-          </div>
+        {pathname === "/" ? (
+          loading ? (
+            <div className="flex items-center justify-center h-5/6">
+              <Loader size="48" />
+            </div>
+          ) : (
+            <CountryTable countries={countries} />
+          )
         ) : (
-          <CountryTable countries={countries} />
+          <Outlet />
         )}
       </Paper>
     </div>
